617e94e3186f43d91480dc738fd7cc28c61ed1e5
estef144z <144652985+estef144z@users.noreply.github.com>
2023-12-15 23:17:18 -0700

    UsarListaDoble.java

--- UsarListaDoble.java
+++ UsarListaDoble.java
@@ -1,38 +1,28 @@
 public class UsarListaDoble {
     public static void main(String[] args) throws Exception {
         ListaDoble lista = new ListaDoble();
-         // Insertar nodos en la lista
+        // Insertar nodos en la lista
         lista.insertaPrimerNodo("A");
-         lista.insertaAlFinal("B");
+        lista.insertaAlFinal("B");
         lista.insertaAlFinal("C");
         lista.insertaAlFinal("D");
 
         System.out.println("Lista inicial:");
         lista.imprimir();
-        
-          Node nodoEncontrado = lista.BuscarNodoPorValor("A");
-           System.out.println("Nodo encontrado: " + nodoEncontrado.name);
-        
-        // Insertar un nuevo nodo después de un nodo específico
-        String nuevoValor = "E";
-        String nodoAntes = "D";
-        if (lista.InsertarDespuesDeNodo(nodoAntes, nuevoValor)) {
-            System.out.println("Nuevo nodo insertado después de " + nodoAntes + ":");
-            lista.imprimir();
-        } else {
-            System.out.println("No se pudo insertar. Nodo antes de " + nodoAntes + " no encontrado.");
-        }
 
-        // Intercambiar dos nodos
-        String nodo1 = "A";
-        String nodo2 = "C";
-        if (lista.IntercambiarNodos(nodo1, nodo2)) {
-            System.out.println("Nodos intercambiados:");
-            lista.imprimir();
-        } else {
-            System.out.println("No se pudo intercambiar. Al menos uno de los nodos no encontrado.");
-        }
-            
-           
+        // Buscar un nodo por el valor de su campo clave y devolver una referencia
+        Node nodoEncontrado = lista.BuscarNodoPorValor("B");
+        System.out.println("Nodo encontrado: " + nodoEncontrado.name);
+
+        // Buscar un nodo por su campo clave e insertar un nuevo nodo después de él
+        lista.InsertarDespuesDeNodo("B", "E");
+        System.out.println("\nLista después de insertar nuevo nodo:");
+        lista.imprimir();
+
+        // Intercambiar un nodo por otro buscado
+        lista.InterCambiarNodos("C", "A");
+        System.out.println("\nLista después de intercambiar nodos:");
+        lista.imprimir();
+
     }
 }
